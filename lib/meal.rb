7fc243486f34e4f392c38688a2fda962d38ed7f4
Oroko <aoroko09@gmail.com>
2020-10-29 18:07:53 -0400

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,18 @@
-class Meal
+# frozen_string_literal: true
 
+class Meal
+  @@all = []
+  attr_accessor :customer, :waiter, :tip
+
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
